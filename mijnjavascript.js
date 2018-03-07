1da661ea3ae95e791d20469c78affefe8f5984cb
kanedamian <31471558+kanedamian@users.noreply.github.com>
2018-03-07 11:48:53 +0100

    javascript 1.3

--- mijnjavascript.js
+++ mijnjavascript.js
@@ -1,8 +1,8 @@
-var a = 10;
-if(a >9){
-    console.log("Hallo a is groter dan 9 hoor");
+var mijnauto = {
+    merk: "Audi",
+    type: "rs3",
+    aantalwielen: 4,
+    kleur: "blauw"
 }
 
-for(var i = 0; i < 10; i++){
-    console.log("i heeft nu de waarde " + i);
-}
+console.log(mijnauto.kleur);
